c261ffa0a83e69bc46c2b2dffdf2ebdcce5393da
Yash Gangwar <gangwaryash06@gmail.com>
2024-10-30 23:49:44 +0530

    added shift4 defaulf user func

--- src/screens/ManageUsers/ManageUsers.js
+++ src/screens/ManageUsers/ManageUsers.js
@@ -1,9 +1,277 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import client from '../../utils/ApiConfig';
+import {
+  Button,
+  Typography,
+  Divider,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  styled,
+  Chip,
+  ListItemText,
+  Checkbox
+} from '@mui/material';
+import { Height } from '@mui/icons-material';
 
-const ManageUsers = () => {
+// const StyledButtonRed = styled(Button)({
+//     margin: '8px',
+// });
+
+const StyledButtonTrans = styled(Button)({
+  margin: '8px',
+  borderRadius: '12px',
+});
+
+const StyledButtonFill = styled(Button)({
+  margin: '8px',
+  color: '#ffffff',
+  borderColor: '#47bf93',
+  backgroundColor: '#47bf93',
+  borderRadius: '12px',
+  '&:hover': {
+    backgroundColor: '#47bf93', // Prevent background color change on hover
+    color: '#ffffff', // Ensure color remains the same on hover
+  },
+});
+
+const CompactTableContainer = styled(TableContainer)({
+  maxWidth: '100%',
+  margin: 'auto auto',
+});
+
+const CompactTableCell = styled(TableCell)({
+  padding: '8px',
+});
+
+const CompactTableCellHeader = styled(TableCell)({
+  padding: '8px',
+  fontWeight: 'bold',
+});
+
+const ITEM_HEIGHT = 40;
+const ITEM_PADDING_TOP = 3;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      height: 40,
+      width: 200,
+    },
+  },
+};
+
+const ManageUsers = (props) => {
+  const navigate = useNavigate({});
+  const [loading, setLoading] = useState(false);
+  const [approvedAdminUsers, setApprovedAdminUsers] = useState([]);
+  const [nonApprovedAdminUsers, setNonApprovedAdminUsers] = useState([]);
+  const [roles, setRoles] = useState([]);
+  const [selectedRoles, setSelectedRoles] = useState({});
+
+  useEffect(() => {
+    props.setHeaderTitle('Manage Users')
+  }, [])
+
+  const getApprovedAdminUsers = async () => {
+    try {
+      setLoading(true);
+      const result = await client.post('/get-approved-admin-users', {
+        headers: { 'Content-Type': 'application/json' },
+      });
+      setApprovedAdminUsers(result.data.users);
+      setLoading(false);
+    } catch (error) {
+      console.log(`getting approved users error ${error}`);
+    }
+  };
+
+  const getNonApprovedAdminUsers = async () => {
+    try {
+      setLoading(true);
+      const result = await client.post('/get-nonapproved-admin-users', {
+        headers: { 'Content-Type': 'application/json' },
+      });
+      setNonApprovedAdminUsers(result.data.users);
+      setLoading(false);
+    } catch (error) {
+      console.log(`getting non-approved admin users error ${error}`);
+    }
+  }
+
+  const getTenantRoles = async (tenantId) => {
+    try {
+      setLoading(true);
+      const data = { tenantId };
+      const result = await client.post('/get-tenant-roles', data, {
+        headers: { 'Content-Type': 'application/json' },
+      });
+      setRoles(result.data.roles);
+      console.log(result.data.roles);
+      setLoading(false);
+    } catch (error) {
+      console.log(`getting user roles error ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getApprovedAdminUsers();
+    getNonApprovedAdminUsers();
+  }, []);
+
+  const handleRoleChange = (userId, selectedUserRoles) => {
+    setSelectedRoles((prevSelectedRoles) => ({
+      ...prevSelectedRoles,
+      [userId]: selectedUserRoles,
+    }));
+  };
+
+  const updateUserStatus = async (tenantId, userId, newStatus) => {
+    try {
+      setLoading(true);
+      let data = { tenantId: tenantId, userId, rolesAssigned: [], newStatus };
+      if (newStatus === 'approved' && (!selectedRoles[userId] || selectedRoles[userId].length === 0)) {
+        alert('No roles selected! Choose atleast one.');
+        setLoading(false);
+        return;
+      }
+      if (newStatus === 'approved') data = { tenantId: tenantId, userId, rolesAssigned: selectedRoles[userId], newStatus };
+      const result = await client.post('/update-user-status', data, {
+        headers: { 'Content-Type': 'application/json' },
+      });
+      setSelectedRoles((prevSelectedRoles) => ({
+        ...prevSelectedRoles,
+        [userId]: [],
+      }));
+      getApprovedAdminUsers();
+      getNonApprovedAdminUsers();
+      setLoading(false);
+    } catch (error) {
+      console.log(`updating user status error ${error}`);
+      setLoading(false)
+    }
+  };
+
   return (
-    <div>ManageUsers</div>
-  )
-}
+    <div>
+      <div style={{ overflowY: 'auto', maxHeight: 'calc(100vh - 115px)' }}>
+        <Paper elevation={3} style={{ margin: '16px', padding: '10px', borderRadius: '12px' }}>
+          <Typography variant="h5" gutterBottom>
+            Pending Approval Requests
+          </Typography>
+          <Divider style={{ marginBottom: '16px' }} />
+          <CompactTableContainer>
+            <Table size="small">
+              <TableHead>
+                <TableRow>
+                  <CompactTableCellHeader>Tenant Id</CompactTableCellHeader>
+                  <CompactTableCellHeader>Name</CompactTableCellHeader>
+                  <CompactTableCellHeader>Username</CompactTableCellHeader>
+                  <CompactTableCellHeader>Email</CompactTableCellHeader>
+                  <CompactTableCellHeader>Roles</CompactTableCellHeader>
+                  <CompactTableCellHeader>Actions</CompactTableCellHeader>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {nonApprovedAdminUsers.map((user) => (
+                  <TableRow key={user._id}>
+                    <CompactTableCell>{user.tenantId}</CompactTableCell>
+                    <CompactTableCell>{`${user.firstName} ${user.lastName}`}</CompactTableCell>
+                    <CompactTableCell>{user.username}</CompactTableCell>
+                    <CompactTableCell>{user.email}</CompactTableCell>
+                    <CompactTableCell>
+                      <FormControl>
+                        <InputLabel>Select Roles</InputLabel>
+                        <Select
+                          multiple
+                          label="Select Roles"
+                          value={selectedRoles[user._id] || []}
+                          onOpen={() => getTenantRoles(user.tenantId)}
+                          onChange={(e) => handleRoleChange(user._id, e.target.value)}
+                          renderValue={(selected) => selected.map((item) => item.roleName).join(', ')}
+                          style={{ minWidth: '150px', maxWidth: '150px', height: '50px', borderRadius: '12px' }}
+                        >
+                          <MenuItem disabled value="">
+                            <em>Choose at least one...</em>
+                          </MenuItem>
+                          {roles && roles.map((role) => (
+                            <MenuItem key={role._id} value={role}>
+                              <Checkbox
+                                checked={Boolean(selectedRoles[user._id] && selectedRoles[user._id].find(
+                                  (selectedRole) => selectedRole._id === role._id
+                                ))}
+                              />
+                              {role.roleName}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </FormControl>
+                    </CompactTableCell>
+                    <CompactTableCell>
+                      <StyledButtonFill onClick={() => updateUserStatus(user.tenantId, user._id, 'approved')} variant='contained'>Approve</StyledButtonFill>
+                      <StyledButtonTrans onClick={() => updateUserStatus(user.tenantId, user._id, 'declined')} variant='outlined' color='error'>Decline</StyledButtonTrans>
+                    </CompactTableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
 
-export default ManageUsers
+            </Table>
+          </CompactTableContainer>
+        </Paper>
+
+        <Paper elevation={3} style={{ margin: '16px', padding: '10px', borderRadius: '12px' }}>
+          <Typography variant="h5" gutterBottom>
+            Approved Admin Users
+          </Typography>
+          <Divider style={{ marginBottom: '16px' }} />
+          <CompactTableContainer>
+            <Table size="small">
+              <TableHead>
+                <TableRow>
+                  <CompactTableCellHeader>Tenant Id</CompactTableCellHeader>
+                  <CompactTableCellHeader>Name</CompactTableCellHeader>
+                  <CompactTableCellHeader>Username</CompactTableCellHeader>
+                  <CompactTableCellHeader>Email</CompactTableCellHeader>
+                  <CompactTableCellHeader>Roles</CompactTableCellHeader>
+                  <CompactTableCellHeader>Actions</CompactTableCellHeader>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {approvedAdminUsers
+                  .map((user, index) => (
+                    <TableRow key={index}>
+                      <CompactTableCell>{user.tenantId}</CompactTableCell>
+                      <CompactTableCell>{`${user.firstName} ${user.lastName}`}</CompactTableCell>
+                      <CompactTableCell>{user.username}</CompactTableCell>
+                      <CompactTableCell>{user.email}</CompactTableCell>
+                      <CompactTableCell>
+                        {user.roles.map((role, index) => (
+                          <Chip label={role.roleName} key={index} style={{ margin: '3px' }} />
+                        ))}
+                      </CompactTableCell>
+                      <CompactTableCell>
+                        <StyledButtonFill onClick={() => updateUserStatus(user.tenantId, user._id, 'pending_superadmin_approval')} variant='contained'>Non-Approve</StyledButtonFill>
+                        <StyledButtonTrans onClick={() => updateUserStatus(user.tenantId, user._id, 'declined')} variant='outlined' color='error'>Delete</StyledButtonTrans>
+                      </CompactTableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </CompactTableContainer>
+        </Paper>
+
+      </div>
+    </div>
+  );
+};
+
+export default ManageUsers;
--- src/screens/Shift4Management/Shift4Management.js
+++ src/screens/Shift4Management/Shift4Management.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Typography, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Divider, styled } from '@mui/material';
-import axios from 'axios'
-import Constants from 'expo-constants'
-import client from '../../utils/ApiConfig'
+import { Button, Typography, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Divider, IconButton, styled } from '@mui/material';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import axios from 'axios';
+import Constants from 'expo-constants';
+import client from '../../utils/ApiConfig';
 
-const shift4backend = Constants.expoConfig.extra.shiftbackendUrl || "http://localhost:8085"
+const shift4backend = Constants.expoConfig.extra.shiftbackendUrl || "http://localhost:8085";
 
 // Styled components
 const StyledPaper = styled(Paper)(({ theme }) => ({
@@ -26,34 +27,20 @@ const StyledButton = styled(Button)(({ theme }) => ({
   },
 }));
 
-const StyledButtonConfirm = styled(Button)(({ theme }) => ({
-  margin: '8px',
-  borderRadius: '8px',
-  backgroundColor: theme.palette.success.dark,
-  color: '#fff',
-  padding: '6px 12px',
-  fontSize: '0.875rem',
-  '&:hover': {
-    backgroundColor: theme.palette.success.light,
-  },
-}));
-
 const Shift4Management = () => {
-
-  const [allRequests, setAllRequests] = useState([])
+  const [allRequests, setAllRequests] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [activeConfirm, setActiveConfirm] = useState({ onboard: false, deboard: false });
 
   const getAllSubscriptionStatus = async () => {
     setLoading(true);
     try {
-      setLoading(true);
       const result = await axios.post(`${shift4backend}/app/s4/getAllStatus`, {
         headers: { 'Content-Type': 'application/json' },
-      })
-      setAllRequests(result.data.subscriptionStatus)
+      });
+      setAllRequests(result.data.subscriptionStatus || []);
     } catch (error) {
-      console.log(`updating invoice status error ${error}`)
+      console.log(`Error updating invoice status: ${error}`);
+    } finally {
       setLoading(false);
     }
   };
@@ -65,20 +52,17 @@ const Shift4Management = () => {
   const OnboardCustomer = async (guid) => {
     setLoading(true);
     try {
-      setLoading(true);
-      const data = {
-        guid: guid
-      }
+      const data = { guid };
       const result = await client.post(`/create-shift4-tenant`, data, {
         headers: { 'Content-Type': 'application/json' },
-      })
+      });
       if (result.data.success) {
-        setLoading(false)
-        await getAllSubscriptionStatus()
-        alert("Tenant onboarded successfully!")
+        await getAllSubscriptionStatus();
+        alert("Tenant onboarded successfully!");
       }
     } catch (error) {
-      console.log(`onboarding shift4 customer error: ${error}`)
+      console.log(`Onboarding shift4 customer error: ${error}`);
+    } finally {
       setLoading(false);
     }
   };
@@ -86,94 +70,85 @@ const Shift4Management = () => {
   const DeboardCustomer = async (tenantId) => {
     setLoading(true);
     try {
-      setLoading(true);
-      const data = {
-        tenantId: tenantId
-      }
+      const data = { tenantId };
       const result = await client.post(`/mark-tenant-inactive`, data, {
         headers: { 'Content-Type': 'application/json' },
-      })
+      });
       if (result.data.success) {
-        setLoading(false)
-        await getAllSubscriptionStatus()
-        alert("Tenant deboarded successfully!")
+        await getAllSubscriptionStatus();
+        alert("Tenant deboarded successfully!");
       }
     } catch (error) {
-      console.log(`deboarding shift4 customer error: ${error}`)
+      console.log(`Deboarding shift4 customer error: ${error}`);
+    } finally {
       setLoading(false);
     }
-  }
+  };
 
   const ConfirmOnboarding = async (guid, locationId) => {
     setLoading(true);
     try {
-      setLoading(true);
-      const data = {
-        guid: guid,
-        locationId: locationId
-      }
+      const data = { guid, locationId };
       const result = await axios.post(`${shift4backend}/app/s4/confirm/install`, data, {
         headers: { 'Content-Type': 'application/json' },
-      })
+      });
       if (result.data.success) {
-        setLoading(false)
-        await getAllSubscriptionStatus()
-        alert("Confirmed customer onboarding!")
+        await getAllSubscriptionStatus();
+        alert("Confirmed customer onboarding!");
       }
     } catch (error) {
-      console.log(`confirming shift4 customer error: ${error}`)
+      console.log(`Confirming shift4 customer onboarding error: ${error}`);
+    } finally {
       setLoading(false);
     }
-  }
+  };
 
   const ConfirmDeboarding = async (locationId) => {
     setLoading(true);
     try {
-      setLoading(true);
-      const data = {
-        locationId: locationId
-      }
+      const data = { locationId };
       const result = await axios.post(`${shift4backend}/app/s4/confirm/uninstall`, data, {
         headers: { 'Content-Type': 'application/json' },
-      })
+      });
       if (result.data.success) {
-        setLoading(false)
-        await getAllSubscriptionStatus()
-        alert("Confirmed customer deboarding!")
+        await getAllSubscriptionStatus();
+        alert("Confirmed customer deboarding!");
       }
     } catch (error) {
-      console.log(`confirming shift4 customer error: ${error}`)
+      console.log(`Confirming shift4 customer deboarding error: ${error}`);
+    } finally {
       setLoading(false);
     }
-  }
+  };
 
-  const DeleteSubscriptonStatus = async (guid, locationId) => {
+  const DeleteSubscriptionStatus = async (guid, locationId) => {
     setLoading(true);
     try {
-      setLoading(true);
-      const data = {
-        guid: guid,
-        locationId: locationId
-      }
+      const data = { guid, locationId };
       const result = await axios.post(`${shift4backend}/app/s4/deleteSubscriptionStatus`, data, {
         headers: { 'Content-Type': 'application/json' },
-      })
+      });
       if (result.data.success) {
-        setLoading(false)
-        await getAllSubscriptionStatus()
-        alert("Deleted customer status!")
+        await getAllSubscriptionStatus();
+        alert("Deleted customer status!");
       }
     } catch (error) {
-      console.log(`deleting shift4 customer status error: ${error}`)
+      console.log(`Deleting shift4 customer status error: ${error}`);
+    } finally {
       setLoading(false);
     }
-  }
+  };
 
   return (
     <div>
-      {/* Onboard Request Box */}
+      {/* Install Request Box */}
       <StyledPaper elevation={3}>
-        <Typography variant="h6">Install Request</Typography>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+          <Typography variant="h6">Install Request</Typography>
+          <IconButton onClick={() => getAllSubscriptionStatus()} disabled={loading}>
+            <RefreshIcon color="primary" />
+          </IconButton>
+        </div>
         <Divider style={{ marginBottom: '16px' }} />
         <TableContainer>
           <Table size="small">
@@ -196,31 +171,21 @@ const Shift4Management = () => {
                     <TableCell>{item.guid}</TableCell>
                     <TableCell>{item.status}</TableCell>
                     <TableCell>
-                      <>
-                        {item.status === 'Install - InProgress' && (
-                          <StyledButton
-                            onClick={() => OnboardCustomer(item.guid)}
-                            style={{ backgroundColor: 'blue' }}
-                          >
-                            Onboard
-                          </StyledButton>
-                        )}
-                        {item.status === 'Installed' && (
-                          <StyledButton
-                            onClick={() => ConfirmOnboarding(item.guid, item.locationId)}
-                            style={{ backgroundColor: 'green' }}
-                          >
-                            Confirm
-                          </StyledButton>
-                        )}
-                        {item.status === 'Install - Confirmed' && (
-                          <StyledButton
-                            style={{ backgroundColor: 'orange' }}
-                          >
-                            No Action
-                          </StyledButton>
-                        )}
-                      </>
+                      {item.status === 'Install - InProgress' && (
+                        <StyledButton onClick={() => OnboardCustomer(item.guid)} style={{ backgroundColor: 'blue' }}>
+                          Onboard
+                        </StyledButton>
+                      )}
+                      {item.status === 'Installed' && (
+                        <StyledButton onClick={() => ConfirmOnboarding(item.guid, item.locationId)} style={{ backgroundColor: 'green' }}>
+                          Confirm
+                        </StyledButton>
+                      )}
+                      {item.status === 'Install - Confirmed' && (
+                        <StyledButton style={{ backgroundColor: 'orange' }}>
+                          No Action
+                        </StyledButton>
+                      )}
                     </TableCell>
                   </TableRow>
                 ))}
@@ -229,7 +194,7 @@ const Shift4Management = () => {
         </TableContainer>
       </StyledPaper>
 
-      {/* Deboard Request Box */}
+      {/* Uninstall Request Box */}
       <StyledPaper elevation={3}>
         <Typography variant="h6">Uninstall Request</Typography>
         <Divider style={{ marginBottom: '16px' }} />
@@ -246,7 +211,7 @@ const Shift4Management = () => {
             </TableHead>
             <TableBody>
               {allRequests
-                .filter(item => item.status === 'Uninstall' || item.status === 'Uninstalled' || item.status === 'Uninstall - Confirmed') // Filter requests where status is either 'Install - InProgress' or 'Install - Confirmed'
+                .filter(item => item.status === 'Uninstall' || item.status === 'Uninstalled' || item.status === 'Uninstall - Confirmed')
                 .map(item => (
                   <TableRow key={item.guid}>
                     <TableCell>{item.locationId}</TableCell>
@@ -254,31 +219,21 @@ const Shift4Management = () => {
                     <TableCell>{item.guid}</TableCell>
                     <TableCell>{item.status}</TableCell>
                     <TableCell>
-                      <>
-                        {item.status === 'Uninstall' && (
-                          <StyledButton
-                            onClick={() => DeboardCustomer(item.tenantId)}
-                            style={{ backgroundColor: 'blue' }}
-                          >
-                            Deboard
-                          </StyledButton>
-                        )}
-                        {item.status === 'Uninstalled' && (
-                          <StyledButton
-                            onClick={() => ConfirmDeboarding(item.locationId)}
-                            style={{ backgroundColor: 'green' }}
-                          >
-                            Confirm
-                          </StyledButton>
-                        )}
-                        {item.status === 'Uninstall - Confirmed' && (
-                          <StyledButton
-                            style={{ backgroundColor: 'orange' }}
-                          >
-                            No Action
-                          </StyledButton>
-                        )}
-                      </>
+                      {item.status === 'Uninstall' && (
+                        <StyledButton onClick={() => DeboardCustomer(item.tenantId)} style={{ backgroundColor: 'blue' }}>
+                          Deboard
+                        </StyledButton>
+                      )}
+                      {item.status === 'Uninstalled' && (
+                        <StyledButton onClick={() => ConfirmDeboarding(item.locationId)} style={{ backgroundColor: 'green' }}>
+                          Confirm
+                        </StyledButton>
+                      )}
+                      {item.status === 'Uninstall - Confirmed' && (
+                        <StyledButton style={{ backgroundColor: 'orange' }}>
+                          No Action
+                        </StyledButton>
+                      )}
                     </TableCell>
                   </TableRow>
                 ))}
@@ -312,14 +267,9 @@ const Shift4Management = () => {
                     <TableCell>{item.guid}</TableCell>
                     <TableCell>{item.status}</TableCell>
                     <TableCell>
-                      <>
-                        <StyledButton
-                          onClick={() => DeleteSubscriptonStatus(item.guid, item.locationId)}
-                          style={{ backgroundColor: 'red' }}
-                        >
-                          Delete
-                        </StyledButton>
-                      </>
+                      <StyledButton onClick={() => DeleteSubscriptionStatus(item.guid, item.locationId)} style={{ backgroundColor: 'red' }}>
+                        Delete Status
+                      </StyledButton>
                     </TableCell>
                   </TableRow>
                 ))}
